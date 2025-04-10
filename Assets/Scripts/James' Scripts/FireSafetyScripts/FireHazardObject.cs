b422f062d33486c19a2bebab8c8ad20ba1d4bd43
LukeN25 <C22532639@mytudublin.ie>
2025-04-10 17:38:56 +0100

    Fire Safety Update
    Each object now has a timer that will trigger an animation once they're done

--- Assets/Scripts/James' Scripts/FireSafetyScripts/FireHazardObject.cs
+++ Assets/Scripts/James' Scripts/FireSafetyScripts/FireHazardObject.cs
@@ -1,9 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 public class FireHazardObject : MonoBehaviour
 {
     public FireHazardManager fireHazardManager;
+    public SpriteRenderer SpriteRenderer;
+    public Animation anim;
 
+    public int hazardTimerInt;
+
+    private void Start()
+    {
+        SpriteRenderer.enabled = false;
+    }
+
+    private void Update()
+    {
+        StartCoroutine(hazardTimer());
+    }
 
     //This variable will be for info popups that will tell the player why what they clicked was a fire safety hazard
     //public GameObject fireHazardInfoText;
@@ -16,5 +30,16 @@ public class FireHazardObject : MonoBehaviour
             fireHazardManager.FoundFireHazard();
             Destroy(this.gameObject);
         }
+    }
+
+    private IEnumerator hazardTimer()
+    {
+        yield return new WaitForSeconds(hazardTimerInt);
+        
+        //Until we have animations we'll just run this code to turn on the sprite
+        SpriteRenderer.enabled = true;
+
+        //This is the actual code for playing the hazard animation
+        //anim.Play();
     }
 }
