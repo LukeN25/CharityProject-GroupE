92eb86b3ce6ba8bf1119a5ecc22f757e26c2a8d1
slademore44 <123640810+slademore44@users.noreply.github.com>
2025-04-14 19:52:11 +0100

    This should fix it

--- Assets/Scripts/Menu Scripts/ReturnToMenu.cs
+++ Assets/Scripts/Menu Scripts/ReturnToMenu.cs
@@ -4,7 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class ReturnToMenu : MonoBehaviour
 {
-    public BirdCounterScript birdCounterScript;
     public string sceneToLoad;
 
     // Update is called once per frame
@@ -14,10 +13,5 @@ public class ReturnToMenu : MonoBehaviour
         {
             SceneManager.LoadScene(sceneToLoad);
         }
-
-        if(birdCounterScript.birdsSnapshotted > 3 && Input.GetKeyDown(KeyCode.Space))
-        {
-            SceneManager.LoadScene(sceneToLoad);
-        } 
     }
 }
